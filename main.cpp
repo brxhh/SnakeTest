79d75f256e959a18c2ae5f1b90be19a85a4f4a5a
stanislav <ctac.samoylenko@gmail.com>
2025-09-22 23:04:12 +0300

    Game Update: - Snake now dies if it collides with itself. - Eating a red apple changes the snake's speed.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <vector>
-
+#include <SFML/System.hpp>
 using namespace std;
 
+sf::Vector2i direction(1, 0);
+sf::Clock gameClock;
 const int width = 40;
 const int height = 20;
 const int blockSize = 16;
@@ -11,12 +13,13 @@ int x = width / 2 - 1;
 int y = height / 2 - 1;
 int fruitX = rand() % width;
 int fruitY = rand() % height;
+int fruitsEaten = 0;
 int score = 0;
-sf::Vector2i direction(1, 0);
-vector<sf::Vector2i> snake; // Vector to store snake blocks
+float moveDelay = 0.12f;
+const float minDelay = 0.05f;
+vector<sf::Vector2i> snake;
 
-void Setup(sf::RenderWindow &window) {
-    window.setFramerateLimit(10);
+void Setup() {
     snake.clear();
     x = width / 2 - 1;
     y = height / 2 - 1;
@@ -29,64 +32,101 @@ void Setup(sf::RenderWindow &window) {
 void Draw(sf::RenderWindow &window) {
     window.clear(sf::Color::Black);
 
-    // Draw the snake
     for (const auto& block : snake) {
         sf::RectangleShape snakeBlock(sf::Vector2f(blockSize, blockSize));
         snakeBlock.setFillColor(sf::Color::Green);
-        snakeBlock.setPosition(block.x * blockSize, block.y * blockSize);
+        snakeBlock.setPosition(sf::Vector2f(block.x * blockSize, block.y * blockSize));
         window.draw(snakeBlock);
     }
 
-    // Draw the fruit
     sf::RectangleShape fruit(sf::Vector2f(blockSize, blockSize));
     fruit.setFillColor(sf::Color::Red);
-    fruit.setPosition(fruitX * blockSize, fruitY * blockSize);
+    fruit.setPosition(sf::Vector2f(fruitX * blockSize, fruitY * blockSize));
     window.draw(fruit);
 
     window.display();
 }
 
 void Input(sf::RenderWindow &window) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed)
+    while (true) {
+        auto eventOpt = window.pollEvent();
+        if (!eventOpt.has_value())
+            break;
+        const sf::Event &event = *eventOpt;
+
+        if (event.is<sf::Event::Closed>()) {
             window.close();
-        else if (event.type == sf::Event::KeyPressed) {
-            if (event.key.code == sf::Keyboard::Up && direction.y != 1)
+        }
+        else if (const auto *keyPressed = event.getIf<sf::Event::KeyPressed>()) {
+            auto key = keyPressed->scancode;
+            using Sc = sf::Keyboard::Scancode;
+
+            if (key == Sc::Up && direction.y != 1)
                 direction = sf::Vector2i(0, -1);
-            else if (event.key.code == sf::Keyboard::Down && direction.y != -1)
+            else if (key == Sc::Down && direction.y != -1)
                 direction = sf::Vector2i(0, 1);
-            else if (event.key.code == sf::Keyboard::Left && direction.x != 1)
+            else if (key == Sc::Left && direction.x != 1)
                 direction = sf::Vector2i(-1, 0);
-            else if (event.key.code == sf::Keyboard::Right && direction.x != -1)
+            else if (key == Sc::Right && direction.x != -1)
                 direction = sf::Vector2i(1, 0);
+
+            //Key (keyCode):
+            /*
+            auto keyCode = keyPressed->key;
+            if (keyCode == Key::Up && direction.y != 1)
+                direction = sf::Vector2i(0, -1);
+            else if (keyCode == Key::Down && direction.y != -1)
+                direction = sf::Vector2i(0, 1);
+            else if (keyCode == Key::Left && direction.x != 1)
+                direction = sf::Vector2i(-1, 0);
+            else if (keyCode == Key::Right && direction.x != -1)
+                direction = sf::Vector2i(1, 0);
+            */
         }
     }
 }
 
 void Logic(sf::RenderWindow &window) {
+    if (gameClock.getElapsedTime().asSeconds() < moveDelay)
+        return;
+    gameClock.restart();
+
     x += direction.x;
     y += direction.y;
-    // Check for snake out of bounds
+
     if (x < 0 || x >= width || y < 0 || y >= height) {
         window.close();
         return;
     }
-    snake.insert(snake.begin(), sf::Vector2i(x, y));
+
+    sf::Vector2i newHead(x, y);
+    snake.insert(snake.begin(), newHead);
+
+    for (size_t i = 1; i < snake.size(); i++) {
+        if (snake[i] == newHead) {
+            window.close();
+            return;
+        }
+    }
+
     if (x == fruitX && y == fruitY) {
         score++;
+        fruitsEaten++;
         fruitX = rand() % width;
         fruitY = rand() % height;
+
+        if (fruitsEaten % 3 == 0 && moveDelay > minDelay) {
+            moveDelay -= 0.005f;
+        }
     } else {
         snake.pop_back();
     }
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(width * blockSize, height * blockSize), "Snake Game");
-
-    Setup(window);
-
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(width * blockSize, height * blockSize)), "Snake Game");
+    window.setFramerateLimit(10);
+    Setup();
     while (window.isOpen()) {
         Input(window);
         Logic(window);
